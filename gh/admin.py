9e086917ae96851160d643edc30dab2e4d4ec509
sjuyu <jummy@gmail.com>
2023-02-15 18:57:49 +0700

    upload thu muc ghcp vo github

---
+++ gh/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from. models import ghh
+admin.site.register(ghh)
---
+++ gh/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+class ghh(models.Model):
+       tennn = models.CharField(max_length=800)
+       nghenghiep = models.CharField(max_length=1200)
+       conghieu = models.CharField(max_length=1500)
